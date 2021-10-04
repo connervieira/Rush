fc6b0663b0143c561041e545aa12160a5c2478f6
Conner Vieira <connervieira@tutanota.com>
2021-10-04 01:16:54 -0400

    Initial commit

---
+++ dropauth/account.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php
+include("./authentication.php");
+?>
+<html lang="en">
+    <head>
+        <title>DropAuth - Account</title>
+        <link href="./stylesheets/styles.css" rel="stylesheet">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <h1>Account</h1>
+        <a class="button" href="./signout.php">Sign Out</a>
+    </body>
+</html>
